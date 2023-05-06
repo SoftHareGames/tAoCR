0f684e5f037e12c22c5bfd607332abecd4736f40
SoftHareGames <132371949+SoftHareGames@users.noreply.github.com>
2023-05-06 19:55:53 +0200

    Add files via upload

--- tAoCR/script.js
+++ tAoCR/script.js
@@ -5,3 +5,19 @@ function play() {
 function nextFromIntro() {
     window.location.href = "one.html";
 }
+
+const form = document.querySelector('form');
+form.addEventListener('submit', (event) => {
+  event.preventDefault(); // prevent the form from submitting normally
+  const cheatCodes = event.target.elements.cheatCodes.value;
+  if (cheatCodes === 'SD suger') {
+    console.log("Entered \"TEST AREA\"!");
+    window.location.href = "testArea.html";
+  }
+  else if (cheatCodes === 'exit' && window.location.href.includes('testArea.html')) {
+    console.log("Exiting test area!");
+    window.location.href = "index.html";
+  }else {
+    console.log('Invalid cheat code');
+  }
+});
